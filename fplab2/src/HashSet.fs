ed423f4e7afb8988dd6b401600d50d0245fb4b62
potap <336773@niuitmo.ru>
2024-11-19 18:20:20 +0300

    Initial commit

---
+++ fplab2/src/HashSet.fs
@@ -0,0 +1,111 @@
+﻿namespace fplab2
+
+module HashSet =
+
+    type HashSet<'T when 'T : equality>(capacity: int ) =
+        
+        let mutable table = Array.init capacity (fun _ -> None: option<'T>)
+        let mutable size = 0
+        let loadFactor = 0.75
+        
+        let hashIndex x len = (hash x % len + len) % len
+        
+        
+        let resize () =
+            let newCapacity = table.Length * 2
+            let newTable = Array.init newCapacity (fun _ -> None : 'T option)
+            let rec tryInsert index item =
+                match table.[index] with
+                | Some x when x = item -> () // Already present, do nothing
+                | None ->
+                    newTable.[index] <- Some item
+                    size <- size + 1
+                | _ ->
+                    tryInsert ((index + 1) % size) item
+            for i = 0 to table.Length - 1 do
+                match table.[i] with
+                | Some value -> tryInsert i value
+                | None -> ()
+            table <- newTable
+        
+        static member Default () = HashSet<'T> (16)
+        
+        member this.Capacity = table.Length
+        member this.Add(item: 'T) =
+            if float size / float table.Length >= loadFactor  then resize()
+            let rec tryInsert index =
+                match table.[index] with
+                | Some x when x = item -> () // Already present, do nothing
+                | None ->
+                    table.[index] <- Some item
+                    size <- size + 1
+                | _ ->
+                    tryInsert ((index + 1) % size)
+            let index = hashIndex item  table.Length
+            tryInsert index
+        
+        member this.Contains(item: 'T) =
+            let rec tryFind index =
+                match table.[index] with
+                | Some x when x = item -> true
+                | None -> false
+                | _ -> tryFind ((index + 1) % size)
+            let index = hashIndex item  table.Length
+            tryFind index
+
+        member this.Remove(item: 'T) =
+            let rec tryRemove index =
+                match table.[index] with
+                | Some x when x = item ->
+                    table.[index] <- None
+                    size <- size - 1
+                | Some _ -> tryRemove ((index + 1) % size)
+                | None -> ()
+            let index = hashIndex item table.Length
+            tryRemove index
+        member this.Size = size
+        
+        
+        
+        member this.Print() =
+            table |> Array.choose id |> Array.iter (fun x -> printf "%A " x)
+            printfn ""
+        
+        member this.Iter(f: 'T -> unit) = table |> Array.iter (fun x ->
+            match x with
+            | None -> ()
+            |_ -> f x.Value
+           )
+        member this.IterRev(f : 'T -> unit) = table |> Array.rev |> Array.iter (fun x ->
+            match x with
+            | None -> ()
+            |_ -> f x.Value
+           )
+        static member iter (f: 'T -> unit) (set : HashSet<'T>) = set.Iter(f)
+        
+        static member map (f : 'T -> 'M) (set : HashSet<'T>) =
+            let newSet = HashSet<'M>(set.Capacity)
+            set.Iter(fun x -> newSet.Add(f x))
+            newSet
+        
+        static member filter (f : 'T -> bool) (set : HashSet<'T>) =
+            let newSet = HashSet<'T>(set.Capacity)
+            set.Iter(fun x -> if f x then newSet.Add(x))
+            newSet
+        static member foldl (f : 'M -> 'T -> 'M) (acc :  'M) (set : HashSet<'T>) =
+            let mutable mut_acc = acc
+            let nextAcc x = (mut_acc <- f mut_acc x)
+            set.Iter(nextAcc)
+            mut_acc
+        
+        static member foldr (f : 'M -> 'T -> 'M) (acc :  'M) (set : HashSet<'T>) =
+            let mutable mut_acc = acc
+            let nextAcc x = (mut_acc <- f mut_acc x)
+            set.IterRev(nextAcc)
+            mut_acc
+            
+    let (@) (set1 : HashSet<'T>) (set2 : HashSet<'T>) =
+          let set3 = HashSet<'T>(set1.Capacity + set2.Capacity)
+          set1.Iter(fun (x) -> set3.Add(x))
+          set2.Iter(fun (x) -> set3.Add(x))
+          set3
---
+++ fplab2/test/UnitTest.fs
@@ -0,0 +1,63 @@
+﻿module fplab2.test.UnitTest
+
+open System.Collections.Generic
+open System.Linq
+open NUnit.Framework
+open fplab2.HashSet
+
+
+[<SetUp>]
+let Setup () =
+    ()
+
+[<Test>]
+let TestAdd () =
+    let set = HashSet<int>.Default()
+    set.Add(3)
+    set.Add(3)
+    set.Add(5)
+    Assert.AreEqual(set.Size, 2)
+    Assert.IsTrue(set.Contains(3))
+[<Test>]
+let TestRemove () =
+    let set = HashSet<int>.Default()
+    Assert.AreEqual(set.Size, 0)
+    set.Add(3)
+    set.Add(3)
+    Assert.AreEqual(set.Size, 1)
+    Assert.IsTrue(set.Contains(3))
+    set.Remove(3)
+    Assert.AreEqual(set.Size, 0)
+    Assert.IsFalse(set.Contains(3))
+    set.Remove(4)
+    Assert.AreEqual(set.Size, 0)
+[<Test>]
+let TestConcatenate () =
+    let set1 = HashSet<int>.Default()
+    let set2 = HashSet<int>.Default()
+    let empty_set = HashSet<int>(0)
+    set1.Add(3)
+    set2.Add(5)
+    let set3 = set1 @ set2
+    Assert.AreEqual(set3.Size, 2)
+    Assert.IsTrue(set3.Contains(3))
+    Assert.AreEqual(set3.Size, (set3 @ empty_set).Size)
+    
+[<Test>]
+let TestIter () =
+    let mutable set = HashSet<int>.Default()
+    set.Add(3)
+    set.Add(4)
+    set.Add(5)
+    set.Add(6)
+    set.Add(7)
+    
+    let set_map = set |> HashSet.map (fun x -> x + 1)
+    Assert.AreEqual(set_map.Size, 5)
+    Assert.IsTrue(set_map.Contains(8))
+    
+    let set_filter = set |> HashSet.filter (fun x -> x > 5)
+    Assert.AreEqual(set_filter.Size, 2)
+    
+    let set_sum = set |> HashSet.foldl (+) 0
+    Assert.AreEqual(set_sum, 25)
