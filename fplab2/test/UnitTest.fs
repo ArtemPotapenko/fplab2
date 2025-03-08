af7e53c2923161a2c476da3db20902a524030df8
potap <336773@niuitmo.ru>
2025-03-08 10:47:30 +0300

    delete unused let

--- fplab2/test/UnitTest.fs
+++ fplab2/test/UnitTest.fs
@@ -15,13 +15,14 @@ let TestAdd () =
 [<Fact>]
 let TestRemove () =
     let set = default_set<int> ()
-    let fact1 = set.size = 0
     let set = set |> add 3 |> add 3
     Assert.Equal(set.size, 1)
     Assert.True(set |> contains (3))
+    
     let set = set |> remove 3
     Assert.Equal(set.size, 0)
     Assert.False(set |> contains (3))
+    
     let set2 = set |> remove (4)
     Assert.Equal(set2.size, 0)
 
@@ -30,7 +31,6 @@ let TestConcatenate () =
     let set1 = default_set<int> () |> add 3
     let set2 = default_set<int> () |> add 5
     let empty_set = HashSet<int>(0)
-
     let set3 = set1 @ set2
     Assert.Equal(set3.size, 2)
     Assert.True(set3 |> contains 3)
@@ -54,6 +54,7 @@ let TestFold () =
     let set = default_set<int> () |> add 3 |> add 4 |> add 5 |> add 6 |> add 7
     let set_sum = set |> foldl (+) 0
     Assert.Equal(set_sum, 25)
+    
     let set = default_set<int> () |> add 5 |> add 4
     Assert.Equal(set |> foldr (*) 1, 20)
 
